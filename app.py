7af880e88403e300b034c4046bc69514558d8a66
PascalATAG <pascal.beckmann@at-unternehmensgruppe.de>
2023-09-21 13:13:18 +0200

    Added drawing of tracks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, send_file, request, jsonify
 from connector import MySQLConnector
 
 app = Flask(__name__)
@@ -8,7 +8,8 @@ db = MySQLConnector(user = 'root', password = "Zoralias7", host = "127.0.0.1", d
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    session['user_id'] = 9
+    return render_template('index.html', user=True)
 
 @app.route('/import_files', methods=['GET'])
 def import_files():
@@ -21,7 +22,25 @@ def login():
 
 @app.route('/map', methods=['GET'])
 def map():
-    return render_template('map.html')
+    tracks = db.get_tracks(session['user_id'])
+    return render_template('map.html', tracks=tracks)
+
+@app.route('/leaflet_css')
+def leaflet_css():
+    file_path = './node_modules/leaflet/dist/leaflet.css'
+    return send_file(file_path)
+
+@app.route('/leaflet_js')
+def leaflet_js():
+    file_path = './node_modules/leaflet/dist/leaflet.js'
+    return send_file(file_path)
+
+@app.route('/draw_path', methods=['POST'])
+def draw_path():
+    data = request.get_json()
+    print(data)
+    coordinates = db.get_points(data['tid'])
+    return jsonify(coordinates)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
--- connector.py
+++ connector.py
@@ -17,7 +17,7 @@ class MySQLConnector:
         point_counter = 0
         for filename in os.listdir(path):
             query = "SELECT * FROM track WHERE(dateiname = %s)"
-            track = self.execute_query(query, False, (filename,))
+            track = self.execute_query(query, False, False, (filename,))
             if len(track) > 0:
                 continue
             track_counter += 1
@@ -26,41 +26,56 @@ class MySQLConnector:
             license = metadata[1]
 
             query = "SELECT * FROM person WHERE(nick = %s)"
-            person = self.execute_query(query, False, (nickname,))
+            person = self.execute_query(query, False, False, (nickname,))
             if not(len(person) > 0):
                 query = "INSERT INTO person(nick) VALUES(%s)"
-                pid = self.execute_query(query, True, (nickname,))
+                pid = self.execute_query(query, True, False, (nickname,))
             else:
                 pid = person[0][0]
             
             query = "SELECT * FROM fahrzeug WHERE(polkz = %s)"
-            vehicle = self.execute_query(query, False, (license,))
+            vehicle = self.execute_query(query, False, False, (license,))
             if not(len(vehicle) > 0):
                 query = "INSERT INTO fahrzeug(polkz) VALUES(%s)"
-                fzid = self.execute_query(query, True, (license,))
+                fzid = self.execute_query(query, True, False, (license,))
             else:
                 fzid = vehicle[0][0]
 
             with open(os.path.join(path, filename)) as file:
                 gpxfile = file.read()
                 gpx = gpxpy.parse(gpxfile)
+                point_data = []
                 for track in gpx.tracks:
                     query = "INSERT INTO track(dateiname, pid, fzid) VALUES(%s, %s, %s)"
-                    tid = self.execute_query(query, True, (filename, pid, fzid))
+                    tid = self.execute_query(query, True, False, (filename, pid, fzid))
                     
                     for segment in track.segments:
                         for point in segment.points:
-                            query = "INSERT INTO punkt(lat, lon, ele, dt, tid) VALUES(%s, %s, %s, %s, %s)"
-                            self.execute_query(query, True, (point.latitude, point.longitude, point.elevation, point.time, tid))
+                            point_data.extend(((point.latitude, point.longitude, point.elevation, point.time, tid),))
                             point_counter += 1
 
+                query = "INSERT INTO punkt(lat, lon, ele, dt, tid) VALUES(%s, %s, %s, %s, %s)"
+                self.execute_query(query, True, True, point_data)
+                                   
         return [track_counter, point_counter]
+    
+    def get_tracks(self, user_id):
+        query = "SELECT * FROM track WHERE(pid = %s)"
+        result = self.execute_query(query, False, False, (user_id,))
+        return result
+    
+    def get_points(self, tid):
+        query = "SELECT lat, lon FROM punkt WHERE(tid = %s)"
+        result = self.execute_query(query, False, False, (tid,))
+        return result
 
-    def execute_query(self, query, commit, data):
+    def execute_query(self, query, commit, many, data):
         connection = mysql.connector.connect(**self.config)
         cursor = connection.cursor(prepared = True)
-        if data:
+        if data and not many:
             cursor.execute(query, data)
+        elif data and many:
+            cursor.executemany(query, data)
         else:
             cursor.execute(query)
         if commit:
